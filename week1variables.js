29c0efa08bcfe5c88a708ae7078712a9c8e3efbc
YATHARLA1996 <123050701+YATHARLA1996@users.noreply.github.com>
2023-02-02 21:16:57 -0600

    Update week1variables.js

--- week1variables.js
+++ week1variables.js
@@ -1,3 +1,3 @@
-https://replit.com/@saicharanyathar/HumbleFineCarriers#index.js
-https://replit.com/@saicharanyathar/HumbleFineCarriers#index.js
-https://replit.com/@saicharanyathar/HumbleFineCarriers#index.js
+https://replit.com/@saicharanyathar/HumbleFineCarriers#index.js //variablesweek1
+https://replit.com/@saicharanyathar/HumbleFineCarriers#index.js //variables week 2
+https://replit.com/@saicharanyathar/HumbleFineCarriers#index.js //variables week3
